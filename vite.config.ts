a5fd867ef7e9ee4dfec5bc8145194d015e712974
Ayushman <ayushmansaha917@gmail.com>
2026-07-27 15:49:13 +0530

    deploy config5

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,10 @@ export default defineConfig(({ mode }) => {
 
   return {
     build: {
-      base: "/PG-Management-System",
+      base: "/PG-Management-System/",
       sourcemap: emitSourcemaps ? "inline" : false,
       minify: !emitSourcemaps,
+      chunkSizeWarningLimit: 1000, // raises the warning threshold to 1MB
     },
     plugins: [
       react(),
